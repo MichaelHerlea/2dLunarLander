d7471aa3986c57f2edcd0e4ffb8971aaeaeea3f2
MichaelHerlea <mihaijherlea+github@gmail.com>
2025-07-14 10:33:10 +0200

    Improved HUD and added data collection/graph displaying at the end

--- 2dLander.py
+++ 2dLander.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from matplotlib import pyplot
 
 pygame.init()
 screen_length = 800
@@ -10,15 +11,23 @@ pygame.display.set_caption("2d Lander")
 clock = pygame.time.Clock()
 text_font = pygame.font.SysFont(None, 30)
 
+star_position = []
+for i in range(int((screen_length*screen_width)/1000)):
+    star_position.append([random.randint(0, screen_length), random.randint(0, screen_width-100)])
+
+lunar_lander_image = pygame.image.load("media/LunarLander.png").convert()
+lunar_lander_image.set_colorkey((255, 255, 255))
+
 velocity = [random.randint(0, int(screen_length*4/7)), random.randint(0, int(screen_width*4/7))]
 position = [0, 0]
 fuel_consumption = 0
 throttle = 1
 angle = 0
 
-star_position = []
-for i in range(int((screen_length*screen_width)/1000)):
-    star_position.append([random.randint(0, screen_length), random.randint(0, screen_width-100)])
+time = []
+angle_over_time = []
+fuel_consumption_over_time = []
+throttle_over_time = []
 
 def draw_text(text, font, color, x, y):
     img = font.render(text, True, color)
@@ -59,18 +68,38 @@ while True:
     for i in range(len(star_position)):
         pygame.draw.rect(window, "white", (star_position[i][0], star_position[i][1], 2, 2))
 
-    pygame.draw.rect(window, "red", (position[0], position[1], 10, 10))
+    angle_in_degrees = round(angle*180/math.pi, 1)
 
-    formated_angle = round(angle*180/math.pi, 1)
-    draw_text(f"Angle: {formated_angle}", text_font, "white", 0, 0)
+    lunar_lander_image_rotated = pygame.transform.rotate(lunar_lander_image, 90-angle_in_degrees)
+    window.blit(lunar_lander_image_rotated, (position[0]-20, position[1]-20))
+
+    draw_text(f"Angle: {angle_in_degrees}", text_font, "white", 0, 0)
     draw_text(f"Throttle: {throttle}", text_font, "white", 0, 20)
 
+    #data collection
+    angle_over_time.append(angle_in_degrees)
+    fuel_consumption_over_time.append(fuel_consumption)
+    throttle_over_time.append(throttle)
+
     #wincondition
-    if position[1]+10 >= screen_width-100 and modulus <= 100:
+    if position[1]+20 >= screen_width-100 and modulus <= 100:
         print(f"Succesfull landing (fuel consumed: {fuel_consumption})")
+        for i in range(len(angle_over_time)):
+            time.append(i)
+        pyplot.figure(num = "Data", figsize = (12, 5))
+        pyplot.subplot(1, 3, 1)
+        pyplot.plot(time, angle_over_time)
+        pyplot.title("Angle")
+        pyplot.subplot(1, 3, 2)
+        pyplot.plot(time, fuel_consumption_over_time)
+        pyplot.title("Fuel consumption")
+        pyplot.subplot(1, 3, 3)
+        pyplot.plot(time, throttle_over_time)
+        pyplot.title("Throttle")
         pygame.quit()
+        pyplot.show()
         exit()
-    if position[1]+10 >= screen_width-100 and modulus > 100:
+    if position[1]+20 >= screen_width-100 and modulus > 100:
         print("Failed landing")
         pygame.quit()
         exit()
